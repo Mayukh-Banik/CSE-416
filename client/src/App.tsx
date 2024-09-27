59f93cb08fccd91aa85caeaa9d41f8106f646e44
Joycelyn Xia <joycelyn.xia@stonybrook.edu>
2024-09-27 16:27:52 -0400

    idk

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { Container, Typography, Button } from '@mui/material';
 import WelcomePage from './Components/WelcomePage';
 import RegisterPage from './Components/RegisterPage';
 import LoginPage from './Components/LoginPage';
+import TransactionPage from './Components/TransactionPage';
 
 const App: React.FC = () => {
     return (
@@ -12,6 +13,7 @@ const App: React.FC = () => {
           <Route path="/" element={<WelcomePage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path='/login' element={<LoginPage />} />
+          <Route path='/transaction' element={<TransactionPage />} />
           {/* <Route path='/register' element={<RegisterPage />} /> */}
         </Routes>
       </Router>
