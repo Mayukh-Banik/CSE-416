24eb93e9a4b161aed0518628149941ab786ad490
ty875 <tingyue0313@gmail.com>
2024-09-27 17:23:48 -0400

    sample mainscreen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,9 @@ import { Container, Typography, Button } from '@mui/material';
 import WelcomePage from './Components/WelcomePage';
 import RegisterPage from './Components/RegisterPage';
 import LoginPage from './Components/LoginPage';
+import SettingPage from './Components/SettingPage';
+import Dashboard from './Components/Dashboard';
+
 
 const App: React.FC = () => {
     return (
@@ -13,6 +16,8 @@ const App: React.FC = () => {
           <Route path="/register" element={<RegisterPage />} />
           <Route path='/login' element={<LoginPage />} />
           {/* <Route path='/register' element={<RegisterPage />} /> */}
+          <Route path="/settings" element={<SettingPage />} /> 
+          <Route path="/dashboard" element={<Dashboard />} />  
         </Routes>
       </Router>
     );
---
+++ client/src/Components/SettingPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SettingPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Settings Page</h1>
+            {/* Add more content here */}
+        </div>
+    );
+}
+
+export default SettingPage
