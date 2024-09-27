f2d5a81e42fa38c2e1641f8724c98daf28002edb
ty875 <tingyue0313@gmail.com>
2024-09-27 17:35:53 -0400

    testing

--- client/src/Components/SettingPage.tsx
+++ client/src/Components/SettingPage.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Sidebar from "./Sidebar";
 
 const SettingPage: React.FC = () => {
     return (
         <div>
             <h1>Settings Page</h1>
+            <Sidebar />
             {/* Add more content here */}
         </div>
     );
