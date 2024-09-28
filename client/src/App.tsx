d8a55572905c2406a702b8a6a2c987e0c984439a
JCrossover-14 <110313504+JCrossover-14@users.noreply.github.com>
2024-09-28 01:16:30 -0400

    pulled from dev

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,10 @@ import { Container, Typography, Button } from '@mui/material';
 import WelcomePage from './Components/WelcomePage';
 import RegisterPage from './Components/RegisterPage';
 import LoginPage from './Components/LoginPage';
+import TransactionPage from './Components/TransactionPage';
+//import SettingPage from './Components/SettingPage';
+//import Dashboard from './Components/Dashboard';
+
 
 const App: React.FC = () => {
     return (
@@ -12,11 +16,11 @@ const App: React.FC = () => {
           <Route path="/" element={<WelcomePage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path='/login' element={<LoginPage />} />
+          <Route path="/dashboard" element={<TransactionPage />} /> {/* Add this line */}
           {/* <Route path='/register' element={<RegisterPage />} /> */}
         </Routes>
       </Router>
     );
   };
-  
 
 export default App;
