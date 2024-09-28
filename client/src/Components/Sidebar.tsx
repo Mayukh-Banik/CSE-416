4cd9c8f423d86219f8897eed23f4af57ca462791
JCrossover-14 <110313504+JCrossover-14@users.noreply.github.com>
2024-09-28 01:16:30 -0400

    pulled from dev

--- client/src/Components/Sidebar.tsx
+++ client/src/Components/Sidebar.tsx
@@ -1,8 +1,29 @@
-// filler code
-const Sidebar: React.FC = () => (
-    <div className="sidebar">
-      <h2>filler sidebar</h2>
-    </div>
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Drawer, List, ListItem, ListItemText, ListItemIcon } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import SettingsIcon from '@mui/icons-material/Settings';
+
+const Sidebar: React.FC = () => {
+  return (
+    <Drawer variant="permanent" anchor="left">
+      <List>
+        <ListItem  component={Link} to="/">
+          <ListItemIcon><HomeIcon /></ListItemIcon>
+          <ListItemText primary="Home" />
+        </ListItem>
+        <ListItem  component={Link} to="/dashboard">
+          <ListItemIcon><DashboardIcon /></ListItemIcon>
+          <ListItemText primary="Dashboard" />
+        </ListItem>
+        <ListItem  component={Link} to="/settings">
+          <ListItemIcon><SettingsIcon /></ListItemIcon>
+          <ListItemText primary="Settings" />
+        </ListItem>
+      </List>
+    </Drawer>
   );
+}
 
 export default Sidebar;
