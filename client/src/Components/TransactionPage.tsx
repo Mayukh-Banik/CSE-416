68d63185809e31baf464a9bbd48d8aedbe8e86e2
Joycelyn Xia <joycelyn.xia@stonybrook.edu>
2024-09-29 19:25:18 -0400

    removed extra transactions button on sidebar

--- client/src/Components/TransactionPage.tsx
+++ client/src/Components/TransactionPage.tsx
@@ -17,6 +17,11 @@ const TransactionPage: React.FC = () => {
     const [search, setSearch] = useState("");
     const [dateFilter, setDateFilter] = useState<string>("all");
     const [statusFilter, setStatusFilter] = useState<string>("all");
+    const [sidebarOpen, setSidebarOpen] = useState<boolean>(true); // State to toggle sidebar
+
+    const toggleSidebar = () => {
+      setSidebarOpen((prev) => !prev);
+    };
 
     const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
         setSearch(event.target.value);
@@ -31,6 +36,7 @@ const TransactionPage: React.FC = () => {
     };
 
     return (
+        
         <Box sx={{ display: "flex" }}>
             <CssBaseline />
             <Sidebar />
