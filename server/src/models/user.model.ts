93016fb2b91f9ca36294dbc22bb63e1295f01719
jungsoo <uiorewq9950@gmail.com>
2024-09-29 20:00:37 -0400

    Update file"publicKey" to user schema

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -15,6 +15,7 @@ const UserSchema: Schema<IUser> = new Schema({
   username: { type: String, required: true, unique: true, trim: true, minlength: 3 },
   email: { type: String, required: true, unique: true, trim: true, lowercase: true },
   password: { type: String, required: true, minlength: 6 },
+  publicKey: { type: String, required: true},
   balance: { type: Number, default: 0, min: 0 },
   createdAt: { type: Date, default: Date.now, immutable: true },
 }, { timestamps: true });
