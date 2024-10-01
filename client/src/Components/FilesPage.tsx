19e910c56c764d86c441f4b2409640a1492c0ea5
Joycelyn Xia <joycelyn.xia@stonybrook.edu>
2024-10-01 14:21:43 -0400

    added files page

---
+++ client/src/Components/FilesPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Sidebar from "./Sidebar";
+import { Box } from "@mui/material";
+
+const FilesPage: React.FC = () => {
+    return (
+        <Box>
+            <Sidebar/>
+        </Box>
+    )
+}
+
+export default FilesPage;
