d4d98d8f251fa77185049a9e98b6f104134b3130
Joycelyn Xia <joycelyn.xia@stonybrook.edu>
2024-10-01 14:20:27 -0400

    added mining page

---
+++ client/src/Components/MiningPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Sidebar from "./Sidebar";
+import { Box } from "@mui/material";
+
+const MiningPage: React.FC = () => {
+    return (
+        <Box>
+            <Sidebar/>
+        </Box>
+    )
+}
+
+export default MiningPage;
