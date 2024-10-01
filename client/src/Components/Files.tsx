40a2ee2a34f6b7d3f589d3a7958dc1834854b159
Joycelyn Xia <joycelyn.xia@stonybrook.edu>
2024-10-01 12:46:48 -0400

    added files page

---
+++ client/src/Components/Files.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Sidebar from "./Sidebar";
+
+const Files: React.FC = () => {
+    return (
+        <div>
+            <h1>'upload' files and transaction deals with storage provider here</h1>
+        </div>
+    )
+}
+
+export default Files;
