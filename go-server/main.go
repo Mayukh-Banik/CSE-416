8cbc2824f181c25d5bb5c240a170b17b6519df57
Dianne <dianne.vincent@stonybrook.edu>
2024-10-15 16:23:59 -0400

    Changes made to MondoDb env on go-server

--- go-server/main.go
+++ go-server/main.go
@@ -4,23 +4,46 @@ import (
     "context"
     "log"
     "net/http"
-	
+	"os"	
 
     "go-server/routes"
     "go-server/utils"
 	"github.com/rs/cors"
+	"github.com/joho/godotenv"
 )
 
 func main() {
     // Initialize MongoDB connection
-    mongoURI := "mongodb://localhost:27017" // Update if necessary
-    utils.ConnectMongo(mongoURI)
+    // mongoURI := "mongodb://localhost:27017" // Update if necessary
+	// MONGODB_URI := os.Getenv("MONGODB_URI")
+    // utils.ConnectMongo(MONGODB_URI)
+
+    // defer func() {
+    //     if err := utils.MongoClient.Disconnect(context.TODO()); err != nil {
+    //         log.Fatalf("Error disconnecting from MongoDB: %v", err)
+    //     }
+    // }()
+
+	// Load the .env file to get environment variables
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatalf("Error loading .env file: %v", err)
+    }
+
+    // Initialize MongoDB connection using the environment variable
+    MONGODB_URI := os.Getenv("MONGODB_URI")
+    if MONGODB_URI == "" {
+        log.Fatalf("MONGODB_URI environment variable is not set")
+    }
+
+    utils.ConnectMongo(MONGODB_URI)
 
     defer func() {
         if err := utils.MongoClient.Disconnect(context.TODO()); err != nil {
             log.Fatalf("Error disconnecting from MongoDB: %v", err)
         }
     }()
+
 
     // Initialize routes
     router := routes.InitRoutes()
@@ -35,7 +58,7 @@ func main() {
 
     // Use the CORS handler directly in the `ListenAndServe` function
     log.Println("Server starting on port 8080...")
-    err := http.ListenAndServe(":8080", c.Handler(router)) // Pass the handler here
+    err = http.ListenAndServe(":8080", c.Handler(router)) // Pass the handler here
     if err != nil {
         log.Fatalf("Server failed to start: %v", err)
     }
