f974506d3370f23ee85f43a0d239aa95322ae05e
ty875 <tingyue0313@gmail.com>
2024-12-11 02:28:23 -0500

    m

--- client/src/Components/ProxyPage.tsx
+++ client/src/Components/ProxyPage.tsx
@@ -251,7 +251,6 @@ const ProxyHosts: React.FC = () => {
       alert(`Connected to ${host.location}`);
       if (response.ok) {
         setCurrentIP(host.address);
-        await updateHistoryOnHost(host.peer_id);
       } if (response.ok) {
         setCurrentIP(host.address);
       }
@@ -261,26 +260,7 @@ const ProxyHosts: React.FC = () => {
       console.error('Error notifying backend:', error);
     }
   };
-  const updateHistoryOnHost = async (hostPeerID: string) => {
-    try {
-      const response = await fetch(`http://localhost:8081/update-history?host=${hostPeerID}`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(proxyHistory),
-      });
-
-      if (!response.ok) {
-        throw new Error('Failed to update history on host');
-      }
-
-      console.log('Successfully updated history on host');
-    } catch (error) {
-      console.error('Error updating history on host:', error);
-    }
-  };
-
+  
   const handleAddProxy = async () => {
     if (newProxy.location.trim() === '' || newProxy.price.trim() === '' || newProxy.Statistics.uptime === '' || newProxy.bandwidth.trim() === '') {
       alert('Please fill in all fields.');
